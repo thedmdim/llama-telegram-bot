bc97629a7fb92c3bbaccbf2ac10ea621bc321b69
Dima <dmitruk.dd@tk23.ru>
2023-06-02 09:25:51 +0400

    empty answer response

--- consumer.go
+++ consumer.go
@@ -88,6 +88,7 @@ func ProcessTask(task *queue.Task) {
 	delete := tgbotapi.NewDeleteMessage(task.UserId, task.AnnounceId)
 	bot.Send(delete)
 
+	log.Println("answer >", answer, "<")
 	if strings.TrimSpace(answer) == "" {
 		answer = "Couldn't generate answer, sorry"
 		return
